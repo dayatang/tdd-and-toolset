741fc6be385fe3d65ad42258449c125d5535466e
yyang <gdyangyu@gmail.com>
2020-05-08 16:19:53 +0800

    update

---
+++ src/main/java/yang/yu/tdd/App.java
@@ -0,0 +1,13 @@
+package yang.yu.tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
