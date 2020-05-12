be584838b572c8aadd94faee5cbfc279e94f2be4
yyang <gdyangyu@gmail.com>
2020-05-12 12:22:32 +0800

    update

--- src/main/java/yang/yu/tdd/bank/Account.java
+++ src/main/java/yang/yu/tdd/bank/Account.java
@@ -1,13 +1,18 @@
 package yang.yu.tdd.bank;
 
+//被测对象
 public class Account {
 
+    //内部状态：账户是否被冻结
     private boolean locked = false;
 
+    //内部状态：当前余额
     private int balance = 0;
 
+    //外部协作者：记录每一笔收支
     private Transactions transactions;
 
+    //用于注入外部写作者的方法
     public void setTransactions(Transactions transactions) {
         this.transactions = transactions;
     }
@@ -20,35 +25,46 @@ public class Account {
         return balance;
     }
 
+    //存款工作单元
     public void deposit(int amount) {
+        //失败路径1：账户被冻结时不允许存款
         if (locked) {
             throw new AccountLockedException();
         }
+        //失败路径2：存款金额不是正数时不允许存款
         if (amount <= 0) {
             throw new InvalidAmountException();
         }
-        balance += amount;
-        transactions.add(this, TransactionType.DEBIT, amount);
+        //成功（快乐）路径
+        balance += amount; //存款成功后改变内部状态
+        transactions.add(this, TransactionType.DEBIT, amount); //存款成功后调用外部协作者
     }
 
+    //取款工作单元
     public void withdraw(int amount) {
+        //失败路径1：账户被冻结时不允许取款
         if (locked) {
             throw new AccountLockedException();
         }
+        //失败路径2：取款金额不是正数时不允许取款
         if (amount <= 0) {
             throw new InvalidAmountException();
         }
+        //失败路径3：取款金额超过余额时不允许取款
         if (amount > balance) {
             throw new BalanceInsufficientException();
         }
-        balance -= amount;
-        transactions.add(this, TransactionType.CREDIT, amount);
+        //成功（快乐）路径
+        balance -= amount;   //取款成功后改变内部状态
+        transactions.add(this, TransactionType.CREDIT, amount); //取款成功后调用外部协作者
     }
 
+    //冻结工作单元
     public void lock() {
         locked = true;
     }
 
+    //解冻工作单元
     public void unlock() {
         locked = false;
     }
--- src/test/java/yang/yu/tdd/bank/AccountWithdrawTest2.java
+++ src/test/java/yang/yu/tdd/bank/AccountWithdrawTest2.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class AccountWithdrawTest2 {
 
@@ -14,16 +15,22 @@ public class AccountWithdrawTest2 {
         // 1. 创建被测对象account
         Account account = new Account();
 
-        // 2. 设置被测对象的初始状态并注入协作对象——将账户当前余额设置为10000元
+        // 2. 设置被测对象的初始状态并注入协作对象
+        // 2.1 模拟协作对象并将其注入被测对象
         Transactions transactions = mock(Transactions.class);
         account.setTransactions(transactions);
+        // 2.2 设置被测对象的内部状态——将账户当前余额设置为10000元
         account.deposit(10000);
 
         // 3. 执行测试——从账户中取款2000元
         account.withdraw(2000);
 
-        // 4. 断言测试结果符合预期：账户余额只剩下8000元
+        // 4. 断言测试结果符合预期
+        // 4.1 断言账户余额只剩下8000元
         assertThat(account.getBalance()).isEqualTo(8000);
+        // 4.2 断言以指定的参数集调用了协作对象transactions的add方法。
+        verify(transactions).add(account, TransactionType.CREDIT, 2000);
+
     }
 
 }
