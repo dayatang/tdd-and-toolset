c7ec8702de2b91d962ca4d1edb4cb78d40dcada8
yyang <gdyangyu@gmail.com>
2020-05-12 12:25:57 +0800

    update

--- src/test/java/yang/yu/tdd/bank/AccountLockTest.java
+++ src/test/java/yang/yu/tdd/bank/AccountLockTest.java
@@ -21,8 +21,12 @@ public class AccountLockTest {
 
     @Test
     void shouldBeLockedWhenLockAndUnlockedWhenUnlock() {
+        //断言默认状态是未冻结
+        assertThat(account.isLocked()).isFalse();
+        //断言调用lock()后账户被冻结
         account.lock();
         assertThat(account.isLocked()).isTrue();
+        //断言调用unlock()后账户已解冻
         account.unlock();
         assertThat(account.isLocked()).isFalse();
     }
