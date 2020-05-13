600b3b8836156e8f380722116238e6e2f8d4ca2e
yyang <gdyangyu@gmail.com>
2020-05-13 11:38:33 +0800

    update

--- src/test/java/yang/yu/tdd/bank/AccountWithdrawTest.java
+++ src/test/java/yang/yu/tdd/bank/AccountWithdrawTest.java
@@ -24,6 +24,7 @@ public class AccountWithdrawTest {
         account.deposit(ORIGINAL_BALANCE);
     }
 
+    //账户状态正常，取款金额小于当前余额时取款成功
     @Test
     void shouldSuccess() {
         int amountOfWithdraw = 2000;
@@ -32,6 +33,7 @@ public class AccountWithdrawTest {
         verify(transactions).add(account, TransactionType.CREDIT, amountOfWithdraw);
     }
 
+    //将余额全部取完，也可以取款成功
     @Test
     void shouldSuccessWhenWithdrawAll() {
         account.withdraw(ORIGINAL_BALANCE);
@@ -39,6 +41,7 @@ public class AccountWithdrawTest {
         verify(transactions).add(account, TransactionType.CREDIT, ORIGINAL_BALANCE);
     }
 
+    //账户被冻结，取款应当失败
     @Test
     void shouldFailWhenAccountLocked() {
         account.lock();
@@ -49,15 +52,7 @@ public class AccountWithdrawTest {
         verify(transactions, never()).add(account, TransactionType.CREDIT, 2000);
     }
 
-    @Test
-    void shouldFailWhenBalanceInsufficient() {
-        assertThrows(BalanceInsufficientException.class, () -> {
-            account.withdraw(ORIGINAL_BALANCE + 1);
-        });
-        assertThat(account.getBalance()).isEqualTo(ORIGINAL_BALANCE);
-        verify(transactions, never()).add(account, TransactionType.CREDIT, ORIGINAL_BALANCE + 1);
-    }
-
+    //取款金额是负数，取款应当失败
     @Test
     void shouldFailWhenAmountLessThanZero() {
         assertThrows(InvalidAmountException.class, () -> {
@@ -67,6 +62,7 @@ public class AccountWithdrawTest {
         verify(transactions, never()).add(account, TransactionType.CREDIT, -1);
     }
 
+    //取款金额是0，应当失败
     @Test
     void shouldFailWhenAmountEqualToZero() {
         assertThrows(InvalidAmountException.class, () -> {
@@ -74,5 +70,15 @@ public class AccountWithdrawTest {
         });
         assertThat(account.getBalance()).isEqualTo(ORIGINAL_BALANCE);
         verify(transactions, never()).add(account, TransactionType.CREDIT, ORIGINAL_BALANCE);
+    }
+
+    //余额不足，应当失败
+    @Test
+    void shouldFailWhenBalanceInsufficient() {
+        assertThrows(BalanceInsufficientException.class, () -> {
+            account.withdraw(ORIGINAL_BALANCE + 1);
+        });
+        assertThat(account.getBalance()).isEqualTo(ORIGINAL_BALANCE);
+        verify(transactions, never()).add(account, TransactionType.CREDIT, ORIGINAL_BALANCE + 1);
     }
 }
