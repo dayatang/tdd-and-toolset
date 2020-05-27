fc11078e3d52051f1e35ccc0885e7552b2d09b56
yyang <dayatang@gmail.com>
2020-05-27 18:26:58 +0800

    update

---
+++ src/test/java/yang/yu/tdd/DisplayNameDemoTest.java
@@ -0,0 +1,23 @@
+package yang.yu.tdd;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("A special test case")
+class DisplayNameDemoTest {
+
+    @Test
+    @DisplayName("Custom test name containing spaces")
+    void testWithDisplayNameContainingSpaces() {
+    }
+
+    @Test
+    @DisplayName("╯°□°）╯")
+    void testWithDisplayNameContainingSpecialCharacters() {
+    }
+
+    @Test
+    @DisplayName("😱")
+    void testWithDisplayNameContainingEmoji() {
+    }
+
+}
