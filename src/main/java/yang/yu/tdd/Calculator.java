14175ad5f3e1bbbabdcefd926ab2a80a7cfc76aa
yyang <dayatang@gmail.com>
2020-05-28 12:36:59 +0800

    update

---
+++ src/main/java/yang/yu/tdd/Calculator.java
@@ -0,0 +1,11 @@
+package yang.yu.tdd;
+
+public class Calculator {
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
