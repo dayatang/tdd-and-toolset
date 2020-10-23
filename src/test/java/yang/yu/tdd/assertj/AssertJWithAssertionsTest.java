3561a5e346f8c14d572a30d5f33742be82ab5a9e
yyang <gdyangyu@gmail.com>
2020-10-23 11:24:57 +0800

    update

---
+++ src/test/java/yang/yu/tdd/assertj/AssertJWithAssertionsTest.java
@@ -0,0 +1,40 @@
+package yang.yu.tdd.assertj;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AssertJWithAssertionsTest implements WithAssertions {
+
+    @Test
+    public void testString() {
+        String sut = "I love you baby";
+        assertThat(sut).startsWith("I love")
+                .endsWith("baby")
+                .contains("you")
+                .contains("I", "love", "you")
+                .doesNotContain("You")
+                .containsIgnoringCase("You");
+    }
+
+    @Test
+    public void testInt() {
+        int sut = 3 + 4;
+        assertThat(sut).isEqualTo(7)
+                .isLessThan(8)
+                .isGreaterThan(6)
+                .isGreaterThanOrEqualTo(7);
+    }
+
+    @Test
+    public void testList() {
+        List<String> sut = Arrays.asList("I love you baby".split(" ").clone());
+        assertThat(sut)
+                .containsAnyOf("I", "love")
+                .doesNotContain("Love")
+                .containsExactly("I", "love", "you", "baby")
+                .doesNotContainNull();
+    }
+}
